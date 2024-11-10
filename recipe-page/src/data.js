d2bfd94c9b45b9f7a21981ccf35db2680a16c924
tortaruga <134407215+tortaruga@users.noreply.github.com>
2024-11-10 13:17:50 +0100

    fixing images not showing

--- recipe-page/src/data.js
+++ recipe-page/src/data.js
@@ -1,55 +1,55 @@
 export default [
     {
         id: 1,
-        img: 'src/images/spicy-stew.jpg',
+        img: 'recipe-page/src/images/spicy-stew.jpg',
         name: 'spicy stew with eggplant, sweet peppers, olives, capers and basil leaves',
     },
 
     {
         id: 2,
-        img: 'src/images/mushrooms-and-pepper.jpg',
+        img: 'recipe-page/src/images/mushrooms-and-pepper.jpg',
         name: 'sautéed mushrooms with pumpkin and sweet peppers',
     },
 
     {
         id: 3,
-        img: 'src/images/fried-carp.jpg',
+        img: 'recipe-page/src/images/fried-carp.jpg',
         name: 'fried carp with vegetables',
     },
 
     {
         id: 4,
-        img: 'src/images/chicken-liver.jpg',
+        img: 'recipe-page/src/images/chicken-liver.jpg',
         name: 'fried chicken liver with onions and herbs',
     },
 
     {
         id: 5,
-        img: 'src/images/grilled-salmon.jpg',
+        img: 'recipe-page/src/images/grilled-salmon.jpg',
         name: 'grilled salmon with salad',
     },
 
     {
         id: 6,
-        img: 'src/images/stuffed-zucchini.jpg',
+        img: 'recipe-page/src/images/stuffed-zucchini.jpg',
         name: 'stuffed zucchini with chicken, tomatoes, onion and cheese crust',
     },
 
     {
         id: 7,
-        img: 'src/images/omelette.jpg',
+        img: 'recipe-page/src/images/omelette.jpg',
         name: 'omelette with radish and red onion',
     },
 
     {
         id: 8,
-        img: 'src/images/sushi-rolls.jpg',
+        img: 'recipe-page/src/images/sushi-rolls.jpg',
         name: 'sushi rolls',
     },
 
     {
         id: 9,
-        img: 'src/images/pasta.jpg',
+        img: 'recipe-page/src/images/pasta.jpg',
         name: 'pasta with chicken and tomato sauce',
     },
 ]
